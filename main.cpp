3042fd5cca290ab1837829a0217de1d741770d67
ShZil <shzil333@gmail.com>
2023-06-29 02:46:56 +0300

    looks good. don't forget to update `messageFormat`

--- main.cpp
+++ main.cpp
@@ -43,12 +43,6 @@ data export (to python for rendering)
 */
 
 /*
-    TODO:
-
-
-*/
-
-/*
     Conceptual:
 
 I wanna parse each message (collection of lines) and immediately pass it to DFs' analyses.
@@ -57,7 +51,18 @@ So the whole thing is just one big iteration.
 
 void handleFile(std::string);
 void handleMessage(std::string, std::streampos);
-bool isNewMessage(std::string);
+bool isNewMessage(std::string, int);
+bool isDigit(char); // no need to write mine, `isdigit` already exists. Also look at `atoi`
+bool isSeparator(char, int format);
+
+enum MessageFormat {
+    DMY = 1,
+    MDY = 2,
+    Slashes = 4,
+    Dots = 8,
+    Parentheses = 16,
+    Dash = 32
+};
 
 int main() {
     std::vector<std::string> paths = getPaths("raw/");
@@ -71,6 +76,8 @@ int main() {
 }
 
 void handleFile(std::string path) {
+    int messageFormat = DMY | Slashes | Dash | Parentheses; // later on I'll add automatic format detection. Right now it's hardcoded.
+
     std::string line;
     std::string message = "";
     std::ifstream f(path, std::ios::binary);
@@ -85,13 +92,14 @@ void handleFile(std::string path) {
             line.pop_back(); // remove `\r` characters
         if (line.empty() || line.back() != '\n') // The condition is a tad excessive (no `line` will have a `\n` already), but it's not problematic.
             line += "\n"; // add newline character.
-        if (isNewMessage(line)) {
+        if (isNewMessage(line, messageFormat)) {
             handleMessage(message, start);
             message = ""; // reset the `message` accumulator, to start a new message.
             start = pos; // reset the `start` of that new message to the [byte after last of the last line of the previous message].
         }
         message += line; // add a line to the message
         pos = currentPosition(f); // update the cursor position (placed at the end of `line`)
+        if ((int)pos > 1000) break;
     }
     handleMessage(message, start);
     // TODO: get rid of `message`
@@ -113,9 +121,37 @@ void handleMessage(std::string message, std::streampos startpos) {
     // propagate the message to all the DFs
 }
 
-bool isNewMessage(std::string line) {
-    if (line.length() == 0) return false;
-    return line.at(0) == '!'; // temporary lol
+bool isNewMessage(std::string line, int format) {
+    if (line.length() < 19) return false;
+    if (format & Parentheses) {
+        if (line[0] != '[') return false;
+        line.erase(0, 1);
+    }
+    bool flag = true;
+    flag &= isDigit(line[0]) && isDigit(line[1]) && isSeparator(line[2], format); // /\d\d(\/|\.)/
+    flag &= isDigit(line[3]) && isDigit(line[4]) && isSeparator(line[5], format); // /\d\d(\/|\.)/
+    flag &= isDigit(line[6]) && isDigit(line[7]) && isDigit(line[8]) && isDigit(line[9]); // /\d\d\d\d/
+    flag &= line[10] == ',' && line[11] == ' ';
+    flag &= isDigit(line[12]);
+    flag &= isDigit(line[15]);
+    if (line[13] == ':') {
+        flag &= isDigit(line[14]);
+    } else {
+        flag &= isDigit(line[13]) && line[14] == ':' && isDigit(line[16]);
+    }
+    return flag;
+
+    // check number's ranges (1-12, 1-31)
+}
+
+bool isDigit(char c) {
+    return c <= '9' && c >= '0';
+}
+
+bool isSeparator(char c, int format) {
+    if (format & Slashes) return c == '/';
+    if (format & Dots) return c == '.';
+    throw 33; // invalid format, cannot have neither Slashes nor Dots.
 }
 
 // Also cotinue translating the Python code
