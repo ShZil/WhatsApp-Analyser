be26d9420a3969ad866dccd00c3474295ec72ea1
ShZil <shzil333@gmail.com>
2023-06-30 03:27:11 +0300

    comment

--- utf8.cpp
+++ utf8.cpp
@@ -4,6 +4,7 @@
 
 // from: https://stackoverflow.com/questions/51352190/read-multi-language-file-wchar-t-vs-char
 // not sure whether all this works well. be skeptical
+// consider not using wchar_t and udf-8 decoding if at all possible
 std::string narrow(const std::wstring& wide_string)
 {
     std::wstring_convert <std::codecvt_utf8 <wchar_t>, wchar_t> convert;
